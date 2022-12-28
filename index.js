c1a2ec584a50f9cab61066937b6679b883bfff76
Tai <hoangchitai08c1a@gmail.com>
2022-12-28 12:17:56 +0700

    init nodejs

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const path = require('path');
+const express = require("express");
+// const morgan = require('morgan');
+// const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-app.get('/tin-tuc', (req, res) => {
-    var a = 1
-    var b = 2
+// // HTTP logger
+// app.use(morgan('combined'));
 
-    var c = a + b;
-  res.send('Hello World!')
-})
+// // //Template Engine
+// app.engine('handlebars', handlebars());
+// app.set('view engine', 'handlebars');
+// app.set('views', path.join(__dirname, 'resources/views'));
+
+// console.log ('PATH:', path.join(__dirname, 'resources/views'));
+app.get("/tin-tuc", (req, res) => {
+  var a = 1;
+  var b = 2;
+  var c = a + b;
+  return res.send("ghfgh32423fgh");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
