858cb152bd48b09e99da452096a647a9af7900bc
Tai <hoangchitai08c1a@gmail.com>
2022-12-28 22:47:54 +0700

    Refactor code: create routes, app/controllers folder

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+const route = require("./routes");
 
 // Xử lý static files
 app.use(express.static(path.join(__dirname, "public")));
@@ -28,28 +29,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-console.log("PATH:", path.join(__dirname, "resources/views"));
-
-// Basic routing (request === req, response === res)
-// GET method
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  console.log(req.query);
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-// POST method
-app.post("/search", (req, res) => {
-  console.log(req.body);
-  res.send("");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
