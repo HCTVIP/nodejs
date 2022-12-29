602dc9050ec0067f102993790e48442e91ac102b
Tai <hoangchitai08c1a@gmail.com>
2022-12-29 17:39:41 +0700

    Prettier-Code Formatter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
-const route = require("./routes");
+const route = require('./routes');
 
 // Xử lý static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
@@ -21,17 +21,17 @@ app.use(express.json());
 
 //Template Engine
 app.engine(
-  "hbs",
-  engine({
-    extname: ".hbs",
-  })
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,17 @@
-const newsRouter = require("./news");
-const siteRouter = require("./site");
+const newsRouter = require('./news');
+const siteRouter = require('./site');
 
 function route(app) {
-  // Basic routing (request === req, response === res)
-  // GET method
-  app.use("/news", newsRouter);
-  app.use("/", siteRouter);
+    // Basic routing (request === req, response === res)
+    // GET method
+    app.use('/news', newsRouter);
+    app.use('/', siteRouter);
 
-  // POST method
-  app.post("/search", (req, res) => {
-    console.log(req.body);
-    res.send("");
-  });
+    // POST method
+    app.post('/search', (req, res) => {
+        console.log(req.body);
+        res.send('');
+    });
 }
 
 module.exports = route;
