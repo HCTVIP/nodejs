7f578fea819584efe9e626f06e24754001c912d8
Tai <hoangchitai08c1a@gmail.com>
2022-12-29 18:10:48 +0700

    create model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,20 @@
+const Course = require("../models/Course");
+
 class SiteController {
-    // [GET] /home
-    index(req, res) {
-        res.render('home');
-    }
-    //   [GET] /search
-    search(req, res) {
-        res.render('search');
-    }
+  // [GET] /home
+  index(req, res) {
+    Course.find({}, function (err, courses) {
+      if (!err) {
+        res.json(courses);
+      } else {
+        res.status(400).json({ error: "ERROR" });
+      }
+    });
+  }
+  //   [GET] /search
+  search(req, res) {
+    res.render("search");
+  }
 }
 
 module.exports = new SiteController();
